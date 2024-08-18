763d8e6b9db517e5c5affc7589cdc6e6d962d53a
juliasuzanne <julia.s.grimes@gmail.com>
2024-08-18 13:10:49 -0400

    udemy state, event handler, lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+import "./CSS/App.css";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 export default function App() {
+  const [animals, setAnimals] = useState([]);
+
   const handleClick = () => {
-    console.log("button was clicked!");
+    setAnimals([...animals, getRandomAnimal()]);
   };
 
   return (
-    <div>
-      <button onClick={handleClick}>Add Animal</button>
+    <div className="app">
+      <button onClick={handleClick}>Add Animal:</button>
+      <div className="animal-list">
+        {animals.map((animal, i) => {
+          return <AnimalShow key={i} type={animal} />;
+        })}
+      </div>
     </div>
   );
 }
